ea9befe700fb7698e4b12455a4baf317278be4fa
Scott Hodges <shodges201@gmail.com>
2019-07-12 10:46:31 -0400

    added an onclick

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -6,3 +6,7 @@ var zipCode = "";
 var radius = "";
 
 var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchTerm +"&size=" + numResults + "&apikey=7P9kCFVoWDXeg9UD7nNXS5F0UouZEaxG";
+
+$("#artist-search").on("click", function(){
+    
+})
