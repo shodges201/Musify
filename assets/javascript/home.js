b6ce258600c65c51b67a1aa6914490401a8a94e2
Scott Hodges <shodges201@gmail.com>
2019-07-12 10:58:08 -0400

    added some ids to elements and created div to expand card

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -8,13 +8,14 @@ var radius = "";
 var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchTerm +"&size=" + numResults + "&apikey=7P9kCFVoWDXeg9UD7nNXS5F0UouZEaxG";
 
 $("#artist-search").on("click", function(){
+    var expand = $("<div>").addClass("card-action");
+    $("#centerCard").append(expand);
+})
+
+$("#venue-search").on("click", function(){
 
 })
 
-$("#artist-search").on("click", function(){
+$("#location-search").on("click", function(){
 
-})
-
-$("#artist-search").on("click", function(){
-    
 })
