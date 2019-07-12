d539d14274e2184e8992a4e11c1e6d0aaa0859e7
Scott Hodges <shodges201@gmail.com>
2019-07-12 13:02:39 -0400

    added on click for search button

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -4,15 +4,17 @@ var state = "";
 var city = "";
 var zipCode = "";
 var radius = "";
+var artist= "";
 
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchTerm +"&size=" + numResults + "&apikey=7P9kCFVoWDXeg9UD7nNXS5F0UouZEaxG";
 var expanded = false;
 
 $("#artist-search").on("click", function(){
     console.log("clicked");
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&date=upcoming";
     if(expanded){
         $("#expandCard").remove();
     }
+    //First input field
     var expandCard = $("<div>").addClass("card-action").attr("id", "expandCard");
     var newRow = $("<div>").addClass("row");
     var newCol = $("<form>").addClass("col s12");
@@ -29,21 +31,22 @@ $("#artist-search").on("click", function(){
 
     newRow = $("<div>").addClass("row");
     newCol = $("<form>").addClass("col s4");
-    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "searchBtn");
-
+    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "artistBtn");
 
     newCol.append(btn);
     newRow.append(newCol);
     expandCard.append(newRow);
+    
 
     expanded = true;
 })
 
 $("#venue-search").on("click", function(){
+    var eventQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchTerm +"&size=" + numResults + "&postalcode=" + zipCode + "&stateCode=" + state + "&city=" + city+ "&radius=" + radius + "&apikey=7P9kCFVoWDXeg9UD7nNXS5F0UouZEaxG";
     if(expanded){
         $("#expandCard").remove();
     }
-
+    //location name
     var expandCard = $("<div>").addClass("card-action").attr("id", "expandCard");
     var newRow = $("<div>").addClass("row");
     var newCol = $("<form>").addClass("col s12");
@@ -58,16 +61,23 @@ $("#venue-search").on("click", function(){
     expandCard.append(newRow);
     $("#centerCard").append(expandCard);
 
+    //Second input field
+    newRow = $("<div>").addClass("row");
+    newCol = $("<form>").addClass("col s12");
+    smallerRow = $("<div>").addClass("row");
+    inputRow = $("<div>").addClass("input-field col s12");
+    inputField = $("<input>").attr("type", "text").attr("id", "").addClass("validate").attr("placeholder", "Artist Name");
 
+    //button
     newRow = $("<div>").addClass("row");
     newCol = $("<form>").addClass("col s4");
-    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "searchBtn");
+    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "venueBtn");
 
 
     newCol.append(btn);
     newRow.append(newCol);
     expandCard.append(newRow);
-    
+
     expanded = true;  
 })
 
@@ -88,7 +98,24 @@ $("#location-search").on("click", function(){
     newRow.append(newCol);
     expandCard.append(newRow);
     $("#centerCard").append(expandCard);
+
+    newRow = $("<div>").addClass("row");
+    newCol = $("<form>").addClass("col s4");
+    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "locationBtn");
+
+
+    newCol.append(btn);
+    newRow.append(newCol);
+    expandCard.append(newRow);
+
     expanded = true;
     
 
+})
+
+$(document).on("click", "#artistBtn", function(event){
+    event.preventDefault();
+    console.log("searched");
+    artist = $("#name").val();
+    localStorage.setItem("artistName", artist);
 })
