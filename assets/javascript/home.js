557c83b57eed80ea7f806944cc2d26d201b57c15
Scott Hodges <shodges201@gmail.com>
2019-07-12 12:24:37 -0400

    added button to form

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -10,50 +10,85 @@ var expanded = false;
 
 $("#artist-search").on("click", function(){
     console.log("clicked");
-    if(!expanded){
-        var expandCard = $("<div>").addClass("card-action");
-        var newRow = $("<div>").addClass("row");
-        var newCol = $("<form>").addClass("col s12");
-        var smallerRow = $("<div>").addClass("row");
-        var inputRow = $("<div>").addClass("input-field col s12");
-        var inputField = $("<input>").attr("type", "text").attr("id", "name").addClass("validate").attr("placeholder", "Artist Name");
-
-        inputRow.append(inputField);
-        smallerRow.append(inputRow);
-        newCol.append(smallerRow);
-        newRow.append(newCol);
-        expandCard.append(newRow);
-        $("#centerCard").append(expandCard);
-        expanded = true;
+    if(expanded){
+        $("#expandCard").remove();
     }
+    var expandCard = $("<div>").addClass("card-action").attr("id", "expandCard");
+    var newRow = $("<div>").addClass("row");
+    var newCol = $("<form>").addClass("col s12");
+    var smallerRow = $("<div>").addClass("row");
+    var inputRow = $("<div>").addClass("input-field col s12");
+    var inputField = $("<input>").attr("type", "text").attr("id", "name").addClass("validate").attr("placeholder", "Artist Name");
+
+    inputRow.append(inputField);
+    smallerRow.append(inputRow);
+    newCol.append(smallerRow);
+    newRow.append(newCol);
+    expandCard.append(newRow);
+    $("#centerCard").append(expandCard);
+
+    newRow = $("<div>").addClass("row");
+    newCol = $("<form>").addClass("col s4");
+    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "searchBtn");
+
+
+    newCol.append(btn);
+    newRow.append(newCol);
+    expandCard.append(newRow);
+
+    expanded = true;
 })
 
 $("#venue-search").on("click", function(){
-    if(!expanded){
-        var expandCard = $("<div>").addClass("card-action");
-        var newRow = $("<div>").addClass("row");
-        var newCol = $("<form>").addClass("col s12");
-        var smallerRow = $("<div>").addClass("row");
-        var inputRow = $("<div>").addClass("input-field col s12");
-        var inputField = $("<input>").attr("type", "text").attr("id", "name").addClass("validate").attr("placeholder", "Location");
-
-        inputRow.append(inputField);
-        smallerRow.append(inputRow);
-        newCol.append(smallerRow);
-        newRow.append(newCol);
-        expandCard.append(newRow);
-        $("#centerCard").append(expandCard);
-        expanded = true;
+    if(expanded){
+        $("#expandCard").remove();
     }
+
+    var expandCard = $("<div>").addClass("card-action").attr("id", "expandCard");
+    var newRow = $("<div>").addClass("row");
+    var newCol = $("<form>").addClass("col s12");
+    var smallerRow = $("<div>").addClass("row");
+    var inputRow = $("<div>").addClass("input-field col s12");
+    var inputField = $("<input>").attr("type", "text").attr("id", "name").addClass("validate").attr("placeholder", "Venue");
+
+    inputRow.append(inputField);
+    smallerRow.append(inputRow);
+    newCol.append(smallerRow);
+    newRow.append(newCol);
+    expandCard.append(newRow);
+    $("#centerCard").append(expandCard);
+
+
+    newRow = $("<div>").addClass("row");
+    newCol = $("<form>").addClass("col s4");
+    var btn = $("<button>").addClass("waves-effect waves-light btn").text("Search").attr("id", "searchBtn");
+
+
+    newCol.append(btn);
+    newRow.append(newCol);
+    expandCard.append(newRow);
     
+    expanded = true;  
 })
 
 $("#location-search").on("click", function(){
-    if(!expanded){
-        var expand = $("<div>").addClass("card-action");
-        $("#centerCard").append(expand);
-        var expand = true;
+    if(expanded){
+        $("#expandCard").remove();
     }
+    var expandCard = $("<div>").addClass("card-action").attr("id", "expandCard");
+    var newRow = $("<div>").addClass("row");
+    var newCol = $("<form>").addClass("col s12");
+    var smallerRow = $("<div>").addClass("row");
+    var inputRow = $("<div>").addClass("input-field col s12");
+    var inputField = $("<input>").attr("type", "text").attr("id", "name").addClass("validate").attr("placeholder", "Location");
+
+    inputRow.append(inputField);
+    smallerRow.append(inputRow);
+    newCol.append(smallerRow);
+    newRow.append(newCol);
+    expandCard.append(newRow);
+    $("#centerCard").append(expandCard);
+    expanded = true;
     
 
 })
