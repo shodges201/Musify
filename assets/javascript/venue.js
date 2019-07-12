8d9e0423f53a71e5e06b474095727eee731d278a
JChesh <sfuchesh@hotmail.com>
2019-07-12 14:32:56 -0400

    Venue

---
+++ assets/javascript/venue.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
+
+
+
+
+
+
+ //   $('.tap-target').tapTarget();
+  });
+  
