31f66c1ca46f85926716ed2dc8ca6bb6ef8926c5
Sheriff Alhassan <dayo.hassan@gmail.com>
2019-07-12 11:12:57 -0400

    expand card action

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -6,16 +6,23 @@ var zipCode = "";
 var radius = "";
 
 var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchTerm +"&size=" + numResults + "&apikey=7P9kCFVoWDXeg9UD7nNXS5F0UouZEaxG";
-
+var expanded = false;
 $("#artist-search").on("click", function(){
     var expand = $("<div>").addClass("card-action");
     $("#centerCard").append(expand);
+    var expand = true;
 })
 
 $("#venue-search").on("click", function(){
+    var expand = $("<div>").addClass("card-action");
+    $("#centerCard").append(expand);
+    var expand = true;
 
 })
 
 $("#location-search").on("click", function(){
+    var expand = $("<div>").addClass("card-action");
+    $("#centerCard").append(expand);
+    var expand = true;
 
 })
