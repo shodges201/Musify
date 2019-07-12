7090c20195cf0bb75fa9c843f2e151422b4731a3
Scott Hodges <shodges201@gmail.com>
2019-07-12 10:53:05 -0400

    addde all onClicks

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -8,5 +8,13 @@ var radius = "";
 var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchTerm +"&size=" + numResults + "&apikey=7P9kCFVoWDXeg9UD7nNXS5F0UouZEaxG";
 
 $("#artist-search").on("click", function(){
+
+})
+
+$("#artist-search").on("click", function(){
+
+})
+
+$("#artist-search").on("click", function(){
     
 })
