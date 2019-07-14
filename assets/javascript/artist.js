70aa522ec0c73d009aa6bf372c86cbb354465444
Scott Hodges <shodges201@gmail.com>
2019-07-14 14:11:15 -0400

    getting location info, venue name, event name, start date and time from the api and displaying it to the screen on the artist page

--- assets/javascript/artist.js
+++ assets/javascript/artist.js
@@ -9,31 +9,31 @@ $(document).ready(function(){
     $("#artist-image").attr("src", imageURL);
     if(localStorage.getItem("itunes") !== ""){
         var linkURL = localStorage.getItem("itunes");
-        var link = $("<a>").attr("href", linkURL).text("itunes").attr("target", "_blank").addClass("social-media");
+        var link = $("<a>").attr("href", linkURL).text("itunes").attr("target", "_blank").addClass("social-media").attr("rel", "noopener noreferrer");
         var listItem = $("<li>").append(link);
         $("#social-media-links").append(listItem);
     }
     if(localStorage.getItem("instagram") !== ""){
         var linkURL = localStorage.getItem("instagram");
-        var link = $("<a>").attr("href", linkURL).text("Instagram").attr("target", "_blank").addClass("social-media");
+        var link = $("<a>").attr("href", linkURL).text("Instagram").attr("target", "_blank").addClass("social-media").attr("rel", "noopener noreferrer");
         var listItem = $("<li>").append(link);
         $("#social-media-links").append(listItem);
     }
     if(localStorage.getItem("twitter") !== ""){
         var linkURL = localStorage.getItem("twitter");
-        var link = $("<a>").attr("href", linkURL).text("Twitter").attr("target", "_blank").addClass("social-media");
+        var link = $("<a>").attr("href", linkURL).text("Twitter").attr("target", "_blank").addClass("social-media").attr("rel", "noopener noreferrer");
         var listItem = $("<li>").append(link);
         $("#social-media-links").append(listItem);
     }
     if(localStorage.getItem("youtube") !== ""){
         var linkURL = localStorage.getItem("youtube");
-        var link = $("<a>").attr("href", linkURL).text("Youtube").attr("target", "_blank").addClass("social-media");
+        var link = $("<a>").attr("href", linkURL).text("Youtube").attr("target", "_blank").addClass("social-media").attr("rel", "noopener noreferrer");
         var listItem = $("<li>").append(link);
         $("#social-media-links").append(listItem);
     }
     if(localStorage.getItem("facebook") !== ""){
         var linkURL = localStorage.getItem("facebook");
-        var link = $("<a>").attr("href", linkURL).text("Facebook").attr("target", "_blank").addClass("social-media");
+        var link = $("<a>").attr("href", linkURL).text("Facebook").attr("target", "_blank").addClass("social-media").attr("rel", "noopener noreferrer");
         var listItem = $("<li>").append(link);
         $("#social-media-links").append(listItem);
     }
@@ -45,8 +45,26 @@ $(document).ready(function(){
     }).then(function(response) {
         json = response;
         $("#shows-container").empty();
-        var newContent = $("<div>").text(JSON.stringify(response));
-        $("#shows-container").append(newContent);
+        //var newContent = $("<div>").text(JSON.stringify(response));
+        //$("#shows-container").append(newContent);
+        for(var i = 0; i < response._embedded.events.length; i++){
+            var startDate = response._embedded.events[i].dates.start.localDate;
+            var startTime = response._embedded.events[i].dates.start.localTime;
+            var eventName = response._embedded.events[i].name;
+            var venueName = response._embedded.events[i]._embedded.venues[0].name;
+            var city = response._embedded.events[i]._embedded.venues[0].city.name;
+            var country = response._embedded.events[i]._embedded.venues[0].country.name;
+            if(response._embedded.events[i]._embedded.venues[0].country.countryCode === "US" || response._embedded.events[i]._embedded.venues[0].country.countryCode === "CA"){
+                var state = response._embedded.events[i]._embedded.venues[0].state.name;
+                var text = $("<p>").text(eventName + " " + venueName + " " + city + ", " + state + ", " + country);
+                $("#shows-container").append(text);
+            }
+            else{
+                var text = $("<p>").text(eventName + " " + venueName + " " + city + ", " + country);
+                $("#shows-container").append(text);
+            }
+            
+        }
     });
 
 
