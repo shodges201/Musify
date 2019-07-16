e691c9fc4d9654ed102b051e0dfc7e5ae72aeadf
Scott Hodges <shodges201@gmail.com>
2019-07-16 13:56:06 -0400

    fixed twitter and changed format of artist results to include date and time

--- assets/javascript/artist.js
+++ assets/javascript/artist.js
@@ -1,6 +1,7 @@
 var artist = "";
 var queryURL = "";
 var json = "";
+
 $(document).ready(function(){
     var artistID = localStorage.getItem("artistID");
     var artistName = localStorage.getItem("artistName");
@@ -21,8 +22,9 @@ $(document).ready(function(){
         $("#social-media-links").append(listItem);
     }
     if(localStorage.getItem("artistTwitter") !== ""){
-        var linkURL = localStorage.getItem("venueTwitter");
-        linkURL = linkURL.replace("https://twitter.com/","")
+        var linkURL = localStorage.getItem("artistTwitter");
+        linkURL = linkURL.replace("https://twitter.com/","");
+        console.log(linkURL);
         var twitterItem = $("<li>").attr("id", "twitter-button");
         $("#social-media-links").append(twitterItem);
         twttr.widgets.createFollowButton(
@@ -66,7 +68,16 @@ $(document).ready(function(){
             for(var i = 0; i < response._embedded.events.length; i++){
                 console.log(response._embedded.events[i]);
                 var startDate = response._embedded.events[i].dates.start.localDate;
+                if(startDate != undefined){
+                    console.log(formatDate(startDate));
+                    startDate = formatDate(startDate);
+                }
                 var startTime = response._embedded.events[i].dates.start.localTime;
+                if(startTime != undefined){
+                    console.log(startTime);
+                    console.log(formatTime(startTime));
+                    startTime = formatTime(startTime);
+                }
                 var eventName = response._embedded.events[i].name;
                 var venueName = response._embedded.events[i]._embedded.venues[0].name;
                 var city = response._embedded.events[i]._embedded.venues[0].city.name;
@@ -80,7 +91,9 @@ $(document).ready(function(){
                      .append($("<td>").append(venueName))
                      .append($("<td>").append(city))
                      .append($("<td>").append(state))
-                     .append($("<td>").append(country)));  
+                     .append($("<td>").append(country))
+                     .append($("<td>").append(startDate))
+                     .append($("<td>").append(startTime)));  
             }
         });
     }
@@ -88,6 +101,32 @@ $(document).ready(function(){
     else{
         $("#shows-container").text("no upcoming events");
     }
+
+    function formatDate(date){
+        date = date.split("-");
+        var str = "";
+        str = date[1] + "/" + date[2] + "/" + date[0];
+        return str;
+    }
+
+    function formatTime(startTime){
+        startTime = startTime.split(":");
+        var str = "";
+        if(parseInt(startTime[0]) > 12){
+            str = (parseInt(startTime[0]) - 12) + ":" + startTime[1] + " pm";
+        }
+        else if(parseInt(startTime[0]) == 12){
+            str = startTime[0] + ":" + startTime[1] + " pm";
+        }
+        else if(parseInt(startTime[0]) == 24){
+            str = startTime[0] + ":" + startTime[1] + " am";
+        }
+        else{
+            str = startTime[0] + ":" + startTime[1] + " am";
+        }
+        return str;
+    }
+
     $(document).on("click", ".show", function(){
         window.open($(this).attr("show-link"));
     })
